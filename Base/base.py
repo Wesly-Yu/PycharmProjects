c2f838ba900dc171572be94d6498f8c586b8c5b1
Wesly-Yu <1633235633@qq.com>
2020-05-11 17:28:41 +0800

    keywords 4

--- Base/base.py
+++ Base/base.py
@@ -9,13 +9,25 @@ class ActionMethod(OperaExcel):
         keyword=self.read_data.get_cell(0, 0)
 
     def execute_keyword(self,keyword,*args):
-        url = args[0]
+        url = str(args[0])
         print(url)
         writeKeyWord=None
         element = None
         if str(keyword).lower()=='open browser':
             writeKeyWord = "cy.visit("+"'"+url+"'"+")"
-
-        if str(keyword).lower() == 'click':
+        elif str(keyword).lower() == 'click':
+            #''''''等待元素显示的默认时间是10秒''''''
             element = args[0]
-            writeKeyWord ="cy.get("+"'"+element+"'"+").click()"
+            timeout = args[1]
+            timeout = str(timeout)
+            if timeout is None:
+                timeout=10000
+            writeKeyWord ="cy.get("+"'"+element+"'"+",{timeout:"+timeout+"}).click()"
+        elif str(keyword).lower() == 'input text':
+            element = args[0]
+            context =str(args[1])
+            writeKeyWord = "cy.get("+"'"+element+"'"+").type("+"'"+context+"'"+")"
+        elif str(keyword).lower() == 'contain':
+            element = args[0]
+            identify = str(args[1])
+            writeKeyWord = "cy.get(" + "'" + element + "'" + ").contain(" + "'" + identify + "'" + ")"
--- utils/Constants.py
+++ utils/Constants.py
@@ -2,5 +2,7 @@
 
 class Constants():
     DefaultSheetNum = 0
-    KeyWordList = ['open browser','click','input text','get value','clear','double click',
-                   'scrol to view','should be','log','clear cookie','contain','get then','reload','and','click picture']
+    KeyWordList = ['open browser','click','input text','clear','double click',
+                   'scrol to view','should','log','clear cookie','contain','get then',
+                   'reload','and','click picture','wait','request','right click','read file',
+                   'equal']
