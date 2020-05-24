2329d0a3aff843671dfd50f33c6b03761e4bf0a8
Wesly-Yu <1633235633@qq.com>
2020-05-24 20:17:34 +0800

    test runmain method-01

--- RunMain/Main.py
+++ RunMain/Main.py
@@ -3,6 +3,7 @@ from utils.ReadExcel import OperaExcel
 from Base.base import ActionMethod
 from utils.CreateJsFile import WriteJsTestFileHead,WriteJsTestFileTail
 from utils.custom_logger import customLogger as lg
+from utils.Constants import Constants
 import logging
 import  pytest
 class TestCase():
@@ -13,9 +14,16 @@ class TestCase():
         super(TestCase, self).__init__()
         self.handle_excel = OperaExcel()
         self.action_method = ActionMethod()
+        self.constants = Constants()
         keyword = self.handle_excel.get_cell_data(0, 0)
     def run_main(self):
-        iterate=self.handle_excel.getTestIterations()
+        iterates=self.handle_excel.getTestIterations()
+        testcaselists=self.handle_excel.getTestCaseName()
+        for ntestCase in range(1,iterates):
+            #将用例列表中的 用例一一取出
+            for testcase in testcaselists:
+                # 用例名称对应的第一个行id
+                nStartStep = self.handle_excel.getRowContains(testcase,self.constants.Col_TestCaseID)
 
 
 if __name__ == '__main__':
