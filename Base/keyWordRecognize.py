21edafe85cfa02d06b93896e5a7a8bd808b650a9
Wesley-Yu <1633235633@qq.com>
2021-01-24 22:09:20 +0800

    add key word two

--- Base/keyWordRecognize.py
+++ Base/keyWordRecognize.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from  Base.buildPythonScript import bulidPythonScript
-
+from  Base.modifyLocator import modify_locator
 
 class ActionMethod():
     logging.info("执行关键字识别")
@@ -12,22 +12,30 @@ class ActionMethod():
             bulidPythonScript.addTestScriptMethod(projectPath,packageName,caseName,writeKeyWord)
         elif str(keyword)== '点击':
             element=str(args[0])
-            writeKeyWord ="self.page.click("+element+")"
+            modify_element=modify_locator(element)
+            writeKeyWord ="self.page.click("+modify_element+")"
             bulidPythonScript.addTestScriptMethod(projectPath, packageName, caseName, writeKeyWord)
         elif str(keyword)== '等待':
-            time = str(args[0])
-            writeKeyWord = "sleep("+time+")"
+            time = str(args[0])+"000"
+            writeKeyWord = "self.page.waitForTimeout("+time+")"
             bulidPythonScript.addTestScriptMethod(projectPath, packageName, caseName, writeKeyWord)
         elif str(keyword)== '输入':
             element=args[0]
+            modify_element = modify_locator(element)
             inputString=str(args[1])
-            cleanWord = "self.page.fill("+element+",'')"
-            writeKeyWord = "self.page.type("+element+","+inputString+")"
+            cleanWord = "self.page.fill("+modify_element+",'')"
+            writeKeyWord = "self.page.type("+modify_element+","+inputString+")"
             bulidPythonScript.addTestScriptMethod(projectPath, packageName, caseName, cleanWord)
             bulidPythonScript.addTestScriptMethod(projectPath, packageName, caseName, writeKeyWord)
-        elif str(keyword)== '下滑':
-
+        elif str(keyword)== '滑动到页面中间':
+            element = args[0]
+            times= args[1]
+            writeKeyWord=self.page.querySelector('a[name="tj_login"]').waitForElementState('visible', 5000)
         elif str(keyword)== '存在':
+
+        elif str(keyword) == '获取文本':
+
+        elif str(keyword) == '存在':
 
         else:
             print("无法执行没有的关键字")
--- Base/play_wright.py
+++ Base/play_wright.py
@@ -1,53 +1,49 @@
 import os
 import unittest
 from time import sleep
+
 from playwright import sync_playwright
-from playwright import async_playwright
-import pytest
-
-# playwright = sync_playwright().start()
-# browser = playwright.chromium.launch(headless=False)
-# page = browser.newPage()
-# page.goto("https://www.baidu.com")
-# page.type("input[id=kw]", "Playwright GitHub")
-# page.click("input[id=su]")
-# sleep(3)
-# page.screenshot(path="example.png")
-# page.close()
-# playwright.stop()
-#
-# playwright = sync_playwright().start()
-# browser = playwright.chromium.launch(headless=False)
-# page = browser.newPage()
-# page.goto("https://www.baidu.com")
-# page.type("input[id=kw]", "Playwright GitHub")
-# page.click("input[id=su]")
-# sleep(3)
-# page.screenshot(path="example.png")
-# page.close()
-# playwright.stop()
 
 
-# playwright = sync_playwright().start()
-# browser = playwright.chromium.launch(headless=False)
-# page = browser.newPage()
 class play_wright(unittest.TestCase):
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch(headless=False)
-    page = browser.newPage()
+    page = browser.newPage(viewport=None)
+
+
+
 
     def test_a(self):
         self.page.goto("https://www.baidu.com")
         self.page.type("input[id=kw]", "Playwright GitHub")
+        self.page.waitForTimeout(1000)
         self.page.click("input[id=su]")
-        sleep(3)
+        self.page.waitForTimeout(3000)
 
 
     def test_c(self):
+        self.page.setViewportSize(width=1920,height=1080)
         self.page.fill("input[id=kw]",'')
         self.page.type("input[id=kw]", "ps5")
+        self.page.waitForTimeout(1000)
         self.page.click("input[id=su]")
-        sleep(3)
+        self.page.waitForTimeout(3000)
+        self.page.keyboard.down(key="ArrowDown")
+        if self.page.waitForSelector("//a[contains(text(),'下一页 >')]", 5000,'visible'):
+            self.page.waitForTimeout(3000)
+            self.page.click("//a[contains(text(),'下一页 >')]")
+        else:
+            self.page.keyboard.down(key="ArrowDown")
+        # self.page.querySelector('a[name="tj_login"]').waitForElementState('visible',5000)
+        # self.page.querySelector('a[name="tj_login"]').click()
+        # self.page.waitForTimeout(3000)
+        self.page.querySelector("//a[contains(text(),'下一页 >')]").scrollIntoViewIfNeeded()
+        # self.page.waitForTimeout(2000)
+        # self.page.querySelector("//a[contains(text(),'下一页 >')]").scrollIntoViewIfNeeded()
+        self.page.waitForTimeout(3000)
+        # if self.page.waitForSelector('a[name="tj_login"]', 5000,'visible'):
+        #     self.page.click('a[name="tj_login"]')
+        #     self.page.waitForTimeout(3000)
         self.page.close()
 
 if __name__ == '__main__':
